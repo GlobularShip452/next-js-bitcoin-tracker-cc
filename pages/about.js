8adeeda42acb53da694b42e00a41563f6ed0a7f3
Yoran <53528221+GlobularShip452@users.noreply.github.com>
2020-12-12 12:09:50 +0100

    main
    main

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+
+const About = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>About BitzPrice</h1>
+      <p>Application to view Bitcoin prices.</p>
+    </div>
+  );
+};
+
+export default About;
